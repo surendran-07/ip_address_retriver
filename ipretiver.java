dd8866f035bd09ed50225e6dc2e587cb98935346
surendran <162984948+surendran-07@users.noreply.github.com>
2024-10-28 16:19:58 +0530

    Create ipretiver.java

---
+++ ipretiver.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+public class ipretiver {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+URL url=new URL("http://www.google.com");
+BufferedReader in=new BufferedReader(new InputStreamReader(url.openStream()));
+String line;
+while ((line =in.readLine())!=null) {
+	System.out.println(line);
+}
+in.close();
+}
+
+}
