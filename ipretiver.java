96dc5e7d9a078891921196b6eb8c8975fd39b7bc
surendran <162984948+surendran-07@users.noreply.github.com>
2024-10-28 16:21:27 +0530

    Update ipretiver.java

--- ipretiver.java
+++ ipretiver.java
@@ -1,17 +1,24 @@
-import java.io.*;
-import java.net.*;
+package sem;
 
-public class ipretiver {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-	public static void main(String[] args) throws Exception{
+public class IPAddressRetriver  {
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-URL url=new URL("http://www.google.com");
-BufferedReader in=new BufferedReader(new InputStreamReader(url.openStream()));
-String line;
-while ((line =in.readLine())!=null) {
-	System.out.println(line);
-}
-in.close();
-}
-
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the website  name");
+		String website=scanner.nextLine();
+		try {
+			InetAddress inetaddress=InetAddress.getByName(website);
+			System.out.println("the rl of"+ website +"is"+inetaddress.getHostAddress());
+		} catch (UnknownHostException e) {
+			System.out.println("unable to retive ip address of the" +website);
+			e.printStackTrace();
+			
+		}
+        scanner.close();
+	}
 }
